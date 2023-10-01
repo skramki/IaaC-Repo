5ebdc731d5d142775950f085ff717384623daa25
skramki <skramki@gmail.com>
2023-10-01 23:45:03 +0800

    Update aws_cloudwatch_alarm.sh
    Updated SNS Topic

--- aws_cloudwatch_alarm.sh
+++ aws_cloudwatch_alarm.sh
@@ -5,9 +5,10 @@
 # Input variable Change:   Value required before executing this script Account ID, SNS Project Name, NOC Email ID
 # Input Variable
 ZONE_ID=ap-southeast-1
-ACC_ID=<AWS_ACCOUNT_ID>
-SNS_PRJ_NAME=<SNS_TOPIC_NAME_CREATED>
+ACC_ID=`aws sts get-caller-identity --query "Account" --output text`
+SNS_PRJ_NAME=<SNS_TOPIC_NAME_CREATED>        # ----> Keyin SNS Topic Name
 ARN_UR="arn:aws:sns:$ZONE_ID:$ACC_ID:$SNS_PRJ_NAME"
+SHRT_NAME=ec2                                # ----> Keyin Instance short name to filter
 TIME_PERIOD_ALARM=300
 DISK_1="/"
 DISK_2="/var"
@@ -20,12 +21,26 @@ CPU_THRESHOLD_CRITICAL=90
 NODE_ALARM_THRESHOLD=0
 RECUR_PERIOD=3
 SEND_EMAIL_ID="YOUREMAIL@DOMAIN.com>
+
+#Create SNS Topic add email subscribtion
+CREATE_SNS() {
+AWS_SNS_LST_BAU=`aws sns list-topics | jq -r '.Topics' | egrep $SNS_PRJ_NAME`
+if [[ "$AWS_SNS_LST_BAU" -eq 0 ]]
+then
+aws sns create-topic --name $SNS_PRJ_NAME
+aws sns subscribe --topic-arn arn:aws:sns:$ZONE_ID:$ACC_ID:$SNS_PRJ_NAME --protocol email --notification-endpoint $SEND_EMAIL_ID
+else
+exit 0
+fi
+}
+CREATE_SNS
+
 # Define the CPU utilization Warning,High,Critical threshold for your alarms
 # Get all EC2 Instance ID in AWS compartment
-aws ec2 describe-instances --filters Name=tag-key,Values=Name --query 'Reservations[*].Instances[*].{Instance:InstanceId,Name:Tags[?Key==`Name`]|[0].Value}' --output text > INSTANCE_NAME.txt
-## Create a CloudWatch alarm for CPU Metrics threshold limits
+aws ec2 describe-instances --filters Name=tag-key,Values=Name --query 'Reservations[*].Instances[*].{Instance:InstanceId,Name:Tags[?Key==`Name`]|[0].Value}' --output text | egrep "ec2|mci" > INSTANCE_NAME.txt
+## create a CloudWatch alarm for CPU, Memory, Disk Utilization threshold limit
 CLOUDWATCH_ALARM_CPU() {
-for INSTANCE_ID in `cat INSTANCE_NAME.txt | egrep "project" | egrep -v "donot|clone|restore|discover"`
+for INSTANCE_ID in `cat INSTANCE_NAME.txt |  egrep -v "donot|clone|restore|discover|test"`
 do
 INSTANCE_NAME=`cat INSTANCE_NAME.txt | egrep -w $INSTANCE_ID | awk '{print $2}'`
 # CloudWatch Alarm for WARNING CPU Utilization > Than $CPU_THRESHOLD_WARNING%
@@ -36,9 +51,8 @@ aws cloudwatch put-metric-alarm --alarm-name "CPUAlarm-$INSTANCE_NAME-$CPU_THRES
 aws cloudwatch put-metric-alarm --alarm-name "CPUAlarm-$INSTANCE_NAME-$CPU_THRESHOLD_CRITICAL" --alarm-description "CPU Utilization Alarm $CPU_THRESHOLD_CRITICAL%" --actions-enabled --metric-name "CPUUtilization" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_CRITICAL --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
 done
 }
-## Create a CloudWatch alarm for Memory Metrics threshold limits
 CLOUDWATCH_ALARM_MEMORY() {
-for INSTANCE_ID in `cat INSTANCE_NAME.txt | egrep "project" | egrep -v "donot|clone|restore|discover"`
+for INSTANCE_ID in `cat INSTANCE_NAME.txt |  egrep -v "donot|clone|restore|discover|test"`
 do
 INSTANCE_NAME=`cat INSTANCE_NAME.txt | egrep -w $INSTANCE_ID | awk '{print $2}'`
 # CloudWatch Alarm for WARNING Memory Utilization > Than $CPU_THRESHOLD_WARNING%
@@ -49,38 +63,28 @@ aws cloudwatch put-metric-alarm --alarm-name "MemoryAlarm-$INSTANCE_NAME-$CPU_TH
 aws cloudwatch put-metric-alarm --alarm-name "MemoryAlarm-$INSTANCE_NAME-$CPU_THRESHOLD_CRITICAL" --alarm-description "Memory Utilization Alarm $CPU_THRESHOLD_CRITICAL%" --actions-enabled --metric-name "Memory-Utilization" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_CRITICAL --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
 done
 }
-## Create a CloudWatch alarm for Linux File System Utilization Metrics threshold limits
 CLOUDWATCH_ALARM_DISK_LINUX() {
-for INSTANCE_ID in `cat INSTANCE_NAME.txt | egrep "project" | egrep -v "donot|clone|restore|discover"`
-do
-for DISK_PATH in $DISK_1 $DISK_2 $DISK_3
+for INSTANCE_ID in `cat INSTANCE_NAME.txt |  egrep -v "donot|clone|restore|discover|test"`
 do
 INSTANCE_NAME=`cat INSTANCE_NAME.txt | egrep -w $INSTANCE_ID | awk '{print $2}'`
 # CloudWatch Alarm Linux for WARNING Disk Utilization > Than $CPU_THRESHOLD_WARNING%
-aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME_$DISK_PATH-$CPU_THRESHOLD_WARNING" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_WARNING%" --actions-enabled --metric-name "Disk-Space-Utilization" --disk-path "$DISK_PATH" --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_WARNING --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
-# CloudWatch Alarm Linux for HIGH Disk Utilization > Than $CPU_THRESHOLD_HIGH%
-aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME_$DISK_PATH-$CPU_THRESHOLD_HIGH" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_HIGH%" --actions-enabled --metric-name "Disk-Space-Utilization" --disk-path "$DISK_PATH" --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_HIGH --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
+aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME-$CPU_THRESHOLD_WARNING" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_WARNING%" --actions-enabled --metric-name "Disk-Space-Utilization" --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_WARNING --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
+# CloudWatch Alarm Linux for WARNING Disk Utilization > Than $CPU_THRESHOLD_HIGH%
+aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME-$CPU_THRESHOLD_HIGH" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_HIGH%" --actions-enabled --metric-name "Disk-Space-Utilization"  --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_HIGH --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
 # CloudWatch Alarm Linux for CRITICAL Disk Utilization > Than $CPU_THRESHOLD_CRITICAL%
-aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME_$DISK_PATH-$CPU_THRESHOLD_CRITICAL" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_CRITICAL%" --actions-enabled --metric-name "Disk-Space-Utilization" --disk-path "$DISK_PATH" --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_CRITICAL --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
-done
+aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME-$CPU_THRESHOLD_CRITICAL" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_CRITICAL%" --actions-enabled --metric-name "Disk-Space-Utilization" --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_CRITICAL --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
 done
 }
-## Create a CloudWatch alarm for Windows Disk Utilization Metrics threshold limits
 CLOUDWATCH_ALARM_DISK_WINDOWS() {
-for INSTANCE_ID in `cat INSTANCE_NAME.txt | egrep "project" | egrep -v "donot|clone|restore|discover"`
+# CloudWatch Alarm Windows for WARNING Disk Utilization > Than 80%
+for INSTANCE_ID in `cat INSTANCE_NAME.txt |  egrep -v "donot|clone|restore|discover|test"`
 do
 INSTANCE_NAME=`cat INSTANCE_NAME.txt | egrep -w $INSTANCE_ID | awk '{print $2}'`
-# CloudWatch Alarm Windows for WARNING Disk Utilization > Than $CPU_THRESHOLD_WARNING%
-aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME_C_DRIVE-$CPU_THRESHOLD_WARNING" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_WARNING%" --actions-enabled --metric-name "Disk-Space-Utilization" --statistic "Average" --objectname "LogicalDisk" --instance "C:" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_WARNING --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
-# CloudWatch Alarm Windows for HIGH Disk Utilization > Than $CPU_THRESHOLD_HIGH%
-aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME_C_DRIVE-$CPU_THRESHOLD_HIGH" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_HIGH%" --actions-enabled --metric-name "Disk-Space-Utilization" --statistic "Average" --objectname "LogicalDisk" --instance "C:" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_HIGH --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
-# CloudWatch Alarm Windows for CRITICAL Disk Utilization > Than $CPU_THRESHOLD_CRITICAL%
-aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME_C_DRIVE-$CPU_THRESHOLD_CRITICAL" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_CRITICAL%" --actions-enabled --metric-name "Disk-Space-Utilization" --statistic "Average" --objectname "LogicalDisk" --instance "C:" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_CRITICAL --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
+aws cloudwatch put-metric-alarm --alarm-name "DiskAlarm-$INSTANCE_NAME-$CPU_THRESHOLD_WARNING" --alarm-description "Disk Utilization Alarm $CPU_THRESHOLD_WARNING%" --actions-enabled --metric-name "Disk-Space-Utilization" --statistic "Average" --namespace "AWS/EC2" --statistic "Average" --period $TIME_PERIOD_ALARM --threshold $CPU_THRESHOLD_WARNING --comparison-operator "GreaterThanThreshold" --evaluation-periods $RECUR_PERIOD --treat-missing-data "breaching" --dimensions "Name=InstanceID,Value=$INSTANCE_ID" --alarm-actions "$ARN_UR"
 done
 }
-## Create a CloudWatch alarm for Instance/Node Status
 CLOUDWATCH_ALARM_NODE() {
-for INSTANCE_ID in `cat INSTANCE_NAME.txt | egrep "project" | egrep -v "donot|clone|restore|discover"`
+for INSTANCE_ID in `cat INSTANCE_NAME.txt |  egrep -v "donot|clone|restore|discover|test"`
 do
 INSTANCE_NAME=`cat INSTANCE_NAME.txt | egrep -w $INSTANCE_ID | awk '{print $2}'`
 # CloudWatch Alarm for Node Status
@@ -90,5 +94,6 @@ done
 CLOUDWATCH_ALARM_CPU
 CLOUDWATCH_ALARM_MEMORY
 CLOUDWATCH_ALARM_DISK_LINUX
+CLOUDWATCH_ALARM_DISK_WINDOWS
 CLOUDWATCH_ALARM_NODE
 rm INSTANCE_NAME.txt
